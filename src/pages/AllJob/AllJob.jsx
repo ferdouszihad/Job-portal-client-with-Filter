f3a3d356b9755f40194dc8b9eafa3842aa7342ce
ferdouszihad <f.zihad333@gmail.com>
2024-12-29 13:25:18 +0600

    final commit

--- src/pages/AllJob/AllJob.jsx
+++ src/pages/AllJob/AllJob.jsx
@@ -1,15 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import useJobs from "../../hooks/useJobs";
 import HotJobCard from "../Home/HotJobCard";
+import { BiSearch } from "react-icons/bi";
 
 const AllJob = () => {
-  const { jobs, loading } = useJobs();
+  const [sort, setSort] = useState(false);
+  const [minSalary, setMinSalary] = useState("");
+  const [maxSalary, setMaxSalary] = useState("");
+  const [search, setSearch] = useState("");
+  const { jobs, loading } = useJobs(sort, search, minSalary, maxSalary);
+
+  //   console.log(sort);
   if (loading) {
     return <h2>Job is Loading</h2>;
   }
   return (
     <div>
       <h1 className=" py-5 text-4xl font-bold text-center">ALL Jobs</h1>
+
+      <div className="w-11/12 mx-auto bg-base-200 py-5 p-3 flex items-center gap-5">
+        <button
+          onClick={() => setSort(!sort)}
+          className={`btn btn-neutral ${sort && "btn-success"}`}
+        >
+          {sort == true ? "Sorted by Salary" : "Sort By Salary"}
+        </button>
+        <BiSearch></BiSearch>
+        <input
+          onKeyUp={(e) => setSearch(e.target.value)}
+          type="text"
+          className="input w-full max-w-2xl"
+          placeholder="Search Jobs by Location"
+        />
+
+        <div className="space-y-3">
+          <input
+            onKeyUp={(e) => setMinSalary(e.target.value)}
+            type="text"
+            className="input w-full max-w-xs"
+            placeholder="Min Salary"
+          />
+          <input
+            onKeyUp={(e) => setMaxSalary(e.target.value)}
+            type="text"
+            className="input w-full max-w-xs"
+            placeholder="Max Salary"
+          />
+        </div>
+      </div>
 
       <div className="grid py-10 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 w-11/12 mx-auto">
         {jobs.map((job) => (
